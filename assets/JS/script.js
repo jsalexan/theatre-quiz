1de6a8b905821bfd3b4b54806f12a5e4ad510c40
Platypus <jennifersalexander@gmail.com>
2022-09-08 14:24:22 -0700

    Added more JS, including working out timer function, variables, question and quiz classes, and setting score functions.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,74 +1,153 @@
-var openingMessage = document.querySelector("#opening");
+var openingMessage = document.querySelector("#opening-display");
 var quizScreen = document.querySelector("quiz-display")
-var startButton = document.getElementById("start-button");
+var questionEl = document.querySelector("question");
+var progressEl = document.querySelector("progress");
+var countdownEl = document.querySelector("countdown")
+var startButton = document.querySelector("start-button");
+var resetButton = document.querySelector("reset-button");
 
+var timer;
+var timerCount;
 
+score = 0
+questionEl = 0
+
+function init() {
+  getScores();
+  openingMessage.setAttribute("display:flex")
+}
+
+// Sets score to 0, prepares questions, and question list to first one.
 class Quiz {
-    constructor(questions) {
-      this.score = 0;
-      this.questions = questions;
-      this.questionList = 0; 
-    }
-  
-    getQuestionList () {
-      return this.questions[this.questionList];
-    }
-  
-    guess(answer) {
-      if (this.getQuestionList().isCorrectAnswer(answer))
-      this.score++;
-      this.questionList++;
-    }
-  
-    isOver() {
-      return this.questionList === this.questions.length;
-      }
-  }
-  
-  class Question {
-    constructor(text, options, answer){
-      this.text = text; 
-      this.options = options; 
-      this.answer = answer;
-    }
-  
-    isCorrectAnswer(choice) {
-      return this.answer === choice;
-    }
+  constructor(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionList = 0; 
   }
 
+  getQuestionList () {
+    return this.questions[this.questionList];
+  }
 
-
-
-
-
-
-
-
-
-
-// Function to start the quiz and move from the opening display.
-  function startQuiz() {
-    // hide start screen
-    openingMessage.setAttribute("class", "hide");
-  
-    // un-hide questions section
-    quizScreen.setAttribute("class", "show");
+//Checks if answer is correct and adjusts the score accordingly and advances to next question. Otherwise it deducts 10 secs off timer
+  guess(answer) {
+    if (this.getQuestionList().isCorrectAnswer(answer)) {
+    this.score++;
+    this.questionList++;}
     
-    startButton.addEventListener("click", showQuestion());
-    console.log("starting quiz");
-  
-   }
+    else if (this.getQuestionList().isWrongAnswer(answer)) {
+    timerCount - 10;
+    this.questionList++;
+  } 
+
+  isOver() {
+    return this.questionList === this.questions.length;
+  }
+    
+}
+
+  class Trivia {
+    constructor(text, options, answer){
+    this.text = text; 
+    this.options = options; 
+    this.answer = answer;
+  }
+
+  isCorrectAnswer(choice) {
+    return this.answer === choice;
+  }
+
+  isWrongAnswer(choice) {
+    return this.answer != choice;
+  }
+}
 
 
-// Function that tracks progress through questions. Will display below answer choice buttons.
-function showProgress() {
-    let currentQuestionNumber = quiz.questionIndex + 1;
-    let progressElement = document.getElementById("progress");
-    progressElement.innerHTML = 
-    `Question ${currentQuestionNumber} of ${quiz.questions.length}`;
-  };
 
 
 
-  
+
+
+
+
+
+// The gameOver function is called when timer reaches 0
+function gameOver() {
+  // wordBlank.textContent = "GAME OVER";
+  startButton.disabled = false;
+  setScores()
+}
+
+
+
+// The setTimer function starts and stops the timer and triggers winGame() and loseGame()
+function startCountdown() {
+  // Sets timer
+  timer = setInterval(function() {
+    timerCount--;
+    countdownEl.textContent = timerCount;
+    if (timerCount >= 0) {
+      // Tests if win condition is met
+      if (isOver && timerCount > 0) {
+        // Clears interval and stops timer
+        clearInterval(timer);
+        gameOver();
+      }
+    }
+    // Tests if time has run out
+    if (timerCount === 0) {
+      // Clears interval
+      clearInterval(timer);
+      gameOver();
+    }
+  }, 1000);
+}
+
+
+
+// Updates win count on screen and sets win count to client storage
+function setScores() {
+  win.textContent = scoreTracker;
+  localStorage.setItem("storedWins", scoreTracker);
+}
+
+
+
+function getScores() {
+  // Get stored value from client storage, if it exists
+  var storedWins = localStorage.getItem("highScores");
+  // If stored value doesn't exist, set counter to 0
+  if (storedWins === null) {
+    scoreTracker = 0;
+  } else {
+    // If a value is retrieved from client storage set the winCounter to that value
+    scoreTracker = storedWins;
+  }
+  //Render win count to page
+  win.textContent = scoreTracker;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+init();
+
+startButton.addEventListener("click", startGame)
+console.log("game started");
--- assets/JS/trivia.js
+++ assets/JS/trivia.js
@@ -1,41 +1,41 @@
 let questions = [
-    new Question(
+    new Trivia(
         "The 80s chart-topper I Know Him So Well was taken from which musical?", ["Evita", "Chess", "Company", "Les Miserables"], "Chess"
     ),
   
-    new Question(
+    new Trivia(
         "Where does Sally Bowles perform in Cabaret?", ["Moulin Rouge", "Mama's", "Kit Kat Klub", "Studio 54"], "Kit Kat Klub"
     ),
   
-    new Question(
+    new Trivia(
         "What was the first Broadway musical with an all-female creative team?", ["A Light in the Piazza", "Fun Home", "Be More Chill", "Waitress"], "Waitress"
     ),
   
-    new Question(
+    new Trivia(
         "Which rock opera did The Who compose?", ["Jesus Christ Superstar", "The Wall", "Tommy", "American Idiot"], "Tommy"
     ),
   
-    new Question(
+    new Trivia(
         "Which Broadway musical is based on the 1924 trials of accused murderers Beulah Annan and Belva Gaertner?", ["Little Shop of Horrors", "Sweeney Todd", "Chicago", "Wicked"], "Chicago"
     ),
   
-    new Question(
+    new Trivia(
         "Which of the following musical hits was written by power duo Richard Rodgers and Oscar Hammerstein", ["Sunday in the Park with George", "Oklahoma", "Phantom of the Opera", "Cats"], "Oklahoma"
     ),
   
-    new Question(
+    new Trivia(
         "What is the name of the carnivorous Venus flytrap plant in Little Shop of Horrors?", ["Seymore", "Mushnik", "Audrey 2", "Mother"], "Audrey 2"
     ),
   
-    new Question(
+    new Trivia(
         "What is Jean Valjean’s prison number in the musical Les Miserables?", ["90210", "#357", "24601", "16740"], "24601"
     ),
   
-    new Question(
+    new Trivia(
         "Who wrote Sunday in the Park with George, Company, Assassins, and Into The Woods?", ["Andrew Lloyd Webber", "Stephen Schwartz", "Stephen Sondheim", "Marvin Hamlisch"], "Stephen Sondheim"
     ),
   
-    new Question(
+    new Trivia(
         "What musical did composer/lyricist Jonathan Larson spend seven years working on, only to die hours before its pre-Broadway opening?", ["Promises, Promises", "Rent", "Hamilton", "Dear Evan Hanson"], "Rent"
     ),
   ];
