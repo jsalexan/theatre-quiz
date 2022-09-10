427f821df0f350570e59567b155eb288c5c42c66
Platypus <jennifersalexander@gmail.com>
2022-09-10 16:31:13 -0700

    Trying to get the question loading to work. I haven't picked the way I want to do this. Review samples from class and see what works best tomorrow.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,83 +1,30 @@
 var openingMessage = document.querySelector("#opening-display");
 var quizScreen = document.querySelector("quiz-display")
 var endDisplay = document.querySelector("end-display")
-var questionEl = document.querySelector(".question");
+
 var startButton = document.querySelector("start-button");
 var resetButton = document.querySelector("reset-button");
+var questionHere = document.querySelector(".question-here");
+var guessResult = document.querySelector(".guess-result");
 
+var btn = document.querySelector(".btn");
+var btn0 = document.querySelector("#Btn0")
+var btn1 = document.querySelector("#Btn1")
+var btn2 = document.querySelector("#Btn2")
+var btn3 = document.querySelector("#Btn3")
 
-
-
-  score = 0;
-  questionsList = 0; 
+score = 0;
+questionsList = 0; 
   
-
-
-
-let questionsList = [
-  {
-      text: "The 80s chart-topper One Night in Bangkok was taken from which musical?", options: ["Evita", "Chess", "Company", "Les Miserables"], answer: "Chess"
-  },
-
-{
-  text: "Where does Sally Bowles perform in Cabaret?", 
-  options: ["Moulin Rouge", "Mama's", "Kit Kat Klub", "Studio 54"], 
-  answer: "Kit Kat Klub"
-},
-
- { 
-  text: "What was the first Broadway musical with an all-female creative team?", options: ["A Light in the Piazza", "Fun Home", "Be More Chill", "Waitress"], answer: "Waitress"
- },
-
- { 
-  text: "Which rock opera did The Who compose?", 
-  options: ["Jesus Christ Superstar", "The Wall", "Tommy", "American Idiot"], answer: "Tommy"
-    },
-
-  { 
-    text: "Which Broadway musical is based on the 1924 trials of accused murderers Beulah Annan and Belva Gaertner?", 
-    options: ["Little Shop of Horrors", "Sweeney Todd", "Chicago", "Wicked"],
-    answer: "Chicago"
-  },
-
-  { 
-    text: "Which of the following musical hits was written by power duo Richard Rodgers and Oscar Hammerstein", 
-    options: ["Sunday in the Park with George", "Oklahoma", "Phantom of the Opera", "Cats"], 
-    answer: "Oklahoma"
-    },
-
-    { 
-      text: "What is the name of the carnivorous Venus flytrap plant in Little Shop of Horrors?", 
-      options: ["Seymore", "Mushnik", "Audrey 2", "Mother"], 
-      answer: "Audrey 2"
-    },
-
-    { 
-      text: "What is Jean Valjean’s prison number in the musical Les Miserables?", 
-      options: ["90210", "#357", "24601", "16740"], 
-      answer: "24601"
-    },
-
-  {
-      text: "Who wrote Sunday in the Park with George, Company, Assassins, and Into The Woods?", 
-      options: ["Andrew Lloyd Webber", "Stephen Schwartz", "Stephen Sondheim", "Marvin Hamlisch"], 
-      answer: "Stephen Sondheim"
-  },
-
-  {
-      text: "What musical did composer/lyricist Jonathan Larson spend seven years working on, only to die hours before its pre-Broadway opening?", 
-      options: ["Promises, Promises", "Rent", "Hamilton", "Dear Evan Hanson"],
-      answer: "Rent"
-  },
-];
 
 function startGame () {
   openingMessage.style.display = "none";
   quizScreen.style.display = "flex";
   questionNumber = 0
   startCountdown();
-  askQuestion(questionNumber);
-
+  askQuestion(questionNumber)
+  console.log("Start game button clicked!");
+}
 
 function showProgress() {
   let currentQuestionNumber = quiz.questionList + 1;
@@ -86,30 +33,16 @@ function showProgress() {
   `Question ${currentQuestionNumber} of ${quiz.questions.length}`;
 }; 
 
-function showQuestion() {
-  if (quiz.isOver()) {
-    showScores();
-  } else {
-    questionEl.innerHTML = quiz.getQuestionList().text;
-
-    let options = quiz.getQuestionList().options;
-    for (let i = 0; i < options.length; i++) {
-      let choiceEl = document.getElementById("option" + i);
-      choiceEl.innerHTML = options[i];
-      guess("btn" + i, options[i]);
+var questionNumber = x;
+function showQuestion(x) {
+        questionHere.textContent = questionsList[x].question;
+        btn0.textContent = questionsList[x].option[0];
+        btn1.textContent = questionsList[x].option[1];
+        btn2.textContent = questionsList[x].option[2];
+        btn3.textContent = questionsList[x].option[3];       
     }
-        showProgress();
-        startCountdown()
-  }
-};
 
- function guess(id, guess) {
-    let button = document.getElementById(id);
-    button.onclick = function() {
-      quiz.guess(guess);
-      showQuestion();
-    }
-  };
+
 
 
 
@@ -148,7 +81,7 @@ function showQuestion() {
 
 
 
-var quiz = new Quiz(questions);
+
 
 
 let time = 90;
@@ -165,7 +98,7 @@ function startCountdown() {
     }, 1000);
 }
 
-// startButton.addEventListener("click", startGame);
+startButton.addEventListener ("click", startGame());
 
 
 
--- assets/JS/trivia.js
+++ assets/JS/trivia.js
@@ -1,41 +1,56 @@
-let questions = [
-    new Trivia(
-        "The 80s chart-topper I Know Him So Well was taken from which musical?", ["Evita", "Chess", "Company", "Les Miserables"], "Chess"
-    ),
+let questionsList = [
+    {
+        text: "1. The 80s chart-topper One Night in Bangkok was taken from which musical?", options: ["Evita", "Chess", "Company", "Les Miserables"], answer: "Chess"
+    },
   
-    new Trivia(
-        "Where does Sally Bowles perform in Cabaret?", ["Moulin Rouge", "Mama's", "Kit Kat Klub", "Studio 54"], "Kit Kat Klub"
-    ),
+  {
+    text: "2. Where does Sally Bowles perform in Cabaret?", 
+    options: ["Moulin Rouge", "Mama's", "Kit Kat Klub", "Studio 54"], 
+    answer: "Kit Kat Klub"
+},
   
-    new Trivia(
-        "What was the first Broadway musical with an all-female creative team?", ["A Light in the Piazza", "Fun Home", "Be More Chill", "Waitress"], "Waitress"
-    ),
+{ 
+text: "3. What was the first Broadway musical with an all-female creative team?", options: ["A Light in the Piazza", "Fun Home", "Be More Chill", "Waitress"], answer: "Waitress"
+},
   
-    new Trivia(
-        "Which rock opera did The Who compose?", ["Jesus Christ Superstar", "The Wall", "Tommy", "American Idiot"], "Tommy"
-    ),
+{ 
+text: "4 Which rock opera did The Who compose?", 
+options: ["Jesus Christ Superstar", "The Wall", "Tommy", "American Idiot"], answer: "Tommy"
+},
   
-    new Trivia(
-        "Which Broadway musical is based on the 1924 trials of accused murderers Beulah Annan and Belva Gaertner?", ["Little Shop of Horrors", "Sweeney Todd", "Chicago", "Wicked"], "Chicago"
-    ),
+{ 
+text: "5. Which Broadway musical is based on the 1924 trials of accused murderers Beulah Annan and Belva Gaertner?", 
+options: ["Little Shop of Horrors", "Sweeney Todd", "Chicago", "Wicked"],
+answer: "Chicago"
+},
   
-    new Trivia(
-        "Which of the following musical hits was written by power duo Richard Rodgers and Oscar Hammerstein", ["Sunday in the Park with George", "Oklahoma", "Phantom of the Opera", "Cats"], "Oklahoma"
-    ),
+{ 
+text: "6. Which of the following musical hits was written by power duo Richard Rodgers and Oscar Hammerstein", 
+options: ["Sunday in the Park with George", "Oklahoma", "Phantom of the Opera", "Cats"], 
+answer: "Oklahoma"
+},
   
-    new Trivia(
-        "What is the name of the carnivorous Venus flytrap plant in Little Shop of Horrors?", ["Seymore", "Mushnik", "Audrey 2", "Mother"], "Audrey 2"
-    ),
+{ 
+text: "7. What is the name of the carnivorous Venus flytrap plant in Little Shop of Horrors?", 
+options: ["Seymore", "Mushnik", "Audrey 2", "Mother"], 
+answer: "Audrey 2"
+},
   
-    new Trivia(
-        "What is Jean Valjean’s prison number in the musical Les Miserables?", ["90210", "#357", "24601", "16740"], "24601"
-    ),
+{ 
+text: "8. What is Jean Valjean’s prison number in the musical Les Miserables?", 
+options: ["90210", "#357", "24601", "16740"], 
+answer: "24601"
+},
   
-    new Trivia(
-        "Who wrote Sunday in the Park with George, Company, Assassins, and Into The Woods?", ["Andrew Lloyd Webber", "Stephen Schwartz", "Stephen Sondheim", "Marvin Hamlisch"], "Stephen Sondheim"
-    ),
+{
+text: "9. Who wrote Sunday in the Park with George, Company, Assassins, and Into The Woods?", 
+options: ["Andrew Lloyd Webber", "Stephen Schwartz", "Stephen Sondheim", "Marvin Hamlisch"], 
+answer: "Stephen Sondheim"
+},
   
-    new Trivia(
-        "What musical did composer/lyricist Jonathan Larson spend seven years working on, only to die hours before its pre-Broadway opening?", ["Promises, Promises", "Rent", "Hamilton", "Dear Evan Hanson"], "Rent"
-    ),
-  ];
+{
+text: "10. What musical did composer/lyricist Jonathan Larson spend seven years working on, only to die hours before its pre-Broadway opening?", 
+options: ["Promises, Promises", "Rent", "Hamilton", "Dear Evan Hanson"],
+answer: "Rent"
+},
+];
