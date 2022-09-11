a2620955b3a32f39c6d8511668cef351b6fd8a79
Platypus <jennifersalexander@gmail.com>
2022-09-11 15:35:07 -0700

    Got the answer options to load! Another naming mistake.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -8,18 +8,19 @@ var questionHere = document.querySelector(".question-here");
 var buttons = document.querySelector(".buttons");
 var guessResult = document.querySelector(".guess-result");
 var quizDeets = document.querySelector(".quiz-deets");
-var progressEl = document.querySelector(".quiz-deets #progress");
+var progressEl = document.querySelector("#progress");
 var countdownEl = document.querySelector(".quiz-deets #countdown");
 
-var btn = document.querySelector(".btn");
-var btn0 = document.querySelector("#Btn0")
-var btn1 = document.querySelector("#Btn1")
-var btn2 = document.querySelector("#Btn2")
-var btn3 = document.querySelector("#Btn3")
+var btn0 = document.querySelector("#btn0");
+var btn1 = document.querySelector("#btn1");
+var btn2 = document.querySelector("#btn2");
+var btn3 = document.querySelector("#btn3");
+var options = document.querySelectorAll(".options");
 
-score = 0;
-questionsList = 0; 
-  
+
+var questionNumber = 0; 
+var finalScore = 0;
+
 
 function startGame () {
   openingMessage.style.display = "none";
@@ -28,18 +29,25 @@ function startGame () {
   quizDeets.style.display = "flex";
   countdownEl.style.display = "flex";
   progressEl.style.display = "flex";
-  questionNumber = 0
   startCountdown();
-//   askQuestion(questionNumber)
+  showQuestion(questionNumber);
   console.log("Start game launched");
 }
 
 function showProgress() {
-  let currentQuestionNumber = quiz.questionList + 1;
+  let currentQuestionsNumber = questionsList + 1;
   let progressEl = document.getElementById("progress");
-  progressEl.innerHTML = 
-  `Question ${currentQuestionNumber} of ${quiz.questions.length}`;
+  progressEl.textContent = `Question ${currentQuestionsNumber}${"of "(questionsList.length)}`;
 }; 
+
+var x = questionNumber
+function showQuestion(x) {
+        questionHere.textContent = questionsList[x].text;
+        btn0.textContent = questionsList[x].options[0];
+        btn1.textContent = questionsList[x].options[1];
+        btn2.textContent = questionsList[x].options[2];
+        btn3.textContent = questionsList[x].options[3];       
+    }
 
 
 let time = 90;
@@ -51,7 +59,7 @@ function startCountdown() {
             showScores();
         } else {
             time--;
-            countdownEl.textContent = "Time left: " + time;
+            countdownEl.textContent = "Time left: " + time + " seconds!";
         }
     }, 1000);
 };
--- assets/JS/trivia.js
+++ assets/JS/trivia.js
@@ -1,55 +1,55 @@
 let questionsList = [
     {
-        text: "1. The 80s chart-topper One Night in Bangkok was taken from which musical?", options: ["Evita", "Chess", "Company", "Les Miserables"], answer: "Chess"
+        text: "The 80s chart-topper One Night in Bangkok was taken from which musical?", options: ["Evita", "Chess", "Company", "Les Miserables"], answer: "Chess"
     },
   
   {
-    text: "2. Where does Sally Bowles perform in Cabaret?", 
+    text: "Where does Sally Bowles perform in Cabaret?", 
     options: ["Moulin Rouge", "Mama's", "Kit Kat Klub", "Studio 54"], 
     answer: "Kit Kat Klub"
 },
   
 { 
-text: "3. What was the first Broadway musical with an all-female creative team?", options: ["A Light in the Piazza", "Fun Home", "Be More Chill", "Waitress"], answer: "Waitress"
+text: "What was the first Broadway musical with an all-female creative team?", options: ["A Light in the Piazza", "Fun Home", "Be More Chill", "Waitress"], answer: "Waitress"
 },
   
 { 
-text: "4 Which rock opera did The Who compose?", 
+text: "Which rock opera did The Who compose?", 
 options: ["Jesus Christ Superstar", "The Wall", "Tommy", "American Idiot"], answer: "Tommy"
 },
   
 { 
-text: "5. Which Broadway musical is based on the 1924 trials of accused murderers Beulah Annan and Belva Gaertner?", 
+text: "Which Broadway musical is based on the 1924 trials of accused murderers Beulah Annan and Belva Gaertner?", 
 options: ["Little Shop of Horrors", "Sweeney Todd", "Chicago", "Wicked"],
 answer: "Chicago"
 },
   
 { 
-text: "6. Which of the following musical hits was written by power duo Richard Rodgers and Oscar Hammerstein", 
+text: "Which of the following musical hits was written by power duo Richard Rodgers and Oscar Hammerstein", 
 options: ["Sunday in the Park with George", "Oklahoma", "Phantom of the Opera", "Cats"], 
 answer: "Oklahoma"
 },
   
 { 
-text: "7. What is the name of the carnivorous Venus flytrap plant in Little Shop of Horrors?", 
+text: "What is the name of the carnivorous Venus flytrap plant in Little Shop of Horrors?", 
 options: ["Seymore", "Mushnik", "Audrey 2", "Mother"], 
 answer: "Audrey 2"
 },
   
 { 
-text: "8. What is Jean Valjean’s prison number in the musical Les Miserables?", 
+text: "What is Jean Valjean’s prison number in the musical Les Miserables?", 
 options: ["90210", "#357", "24601", "16740"], 
 answer: "24601"
 },
   
 {
-text: "9. Who wrote Sunday in the Park with George, Company, Assassins, and Into The Woods?", 
+text: "Who wrote Sunday in the Park with George, Company, Assassins, and Into The Woods?", 
 options: ["Andrew Lloyd Webber", "Stephen Schwartz", "Stephen Sondheim", "Marvin Hamlisch"], 
 answer: "Stephen Sondheim"
 },
   
 {
-text: "10. What musical did composer/lyricist Jonathan Larson spend seven years working on, only to die hours before its pre-Broadway opening?", 
+text: "What musical did composer/lyricist Jonathan Larson spend seven years working on, only to die hours before its pre-Broadway opening?", 
 options: ["Promises, Promises", "Rent", "Hamilton", "Dear Evan Hanson"],
 answer: "Rent"
 },
