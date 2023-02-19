94fe4ff5a3a396df630000a8bf808f4145640ba3
Jennifer Alexander <110498167+jsalexan@users.noreply.github.com>
2023-02-19 00:01:25 -0800

    Fixing broken pathing to highscores page

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -47,7 +47,7 @@ function saveHighScores(event) {
     highScores.splice(5);
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign("/");
+    window.location.assign("https://jsalexan.github.io/theatre-quiz/");
 
 };
 
